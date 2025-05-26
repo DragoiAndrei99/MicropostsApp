b7f14bd4d18089f0bd80bfbde70c883e6ee1f53c
Andrei Dragoi <andrei.dragoi@takeofflabs.com>
2025-05-26 14:50:16 +0300

    mail fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,56 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  attr_accessor :activation_token
+
+  before_save   { self.email = email.downcase }
+  before_create :create_activation_digest
 
   has_many :microposts, dependent: :destroy
-  has_many :comments, dependent: :destroy
-  has_many :notifications, dependent: :destroy
 
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
-  resources :comments
-  resources :notifications
-
-
-  root "home#index"
-  get "help", to: "static_pages#help"
-  get "static_pages/home"
-  get "static_pages/help"
+  get "sessions/new"
   resources :microposts
+  resources :account_activations, only: [:edit]
+  resources :users
+  resources :users do
+    member do
+      get :microposts
+    end
+  end
+  resources :account_activations, only: [:edit]
+
+  root "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
+  get    "/login",  to: "sessions#new"
+  post   "/login",  to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
 end
